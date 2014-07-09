da4fd5536d56123070d22141ca2a42e0c72003cd
emperorliu <mangomystery@gmail.com>
2014-07-09 03:57:49 -0700

    Adding Varibles

---
+++ 02_Variables/name.rb
@@ -0,0 +1,22 @@
+puts 'What is your name?'
+
+name = gets.chomp
+
+puts 'Hello ' + name + ', nice to meet you.'
+
+10.times do |n|
+	puts name
+end
+
+# exercise 4
+
+puts 'What is your first name?'
+
+first_name = gets.chomp
+
+puts 'What is your last name?'
+
+last_name = gets.chomp
+
+puts 'Hi ' + first_name + ' ' + last_name 
+
---
+++ 02_Variables/x.rb
@@ -0,0 +1,12 @@
+# exercise 5
+
+puts 'The first one prints 3, and the second one prints an error 
+because the variable x is not available. The variable x is 
+created within the scope of the do/end block.'
+
+# exercise 6
+
+puts 'The program is trying to reference a variable named shoes that
+has not been defined in the program, or is outside the scope in which
+it is being called'
+
